479083e4934194c71afb2480715e4c0ee84bbda2
Robertas <galaja@inbox.lt>
2022-02-09 21:31:40 +0200

    Task 3 - 02.09

--- Task 3/script.js
+++ Task 3/script.js
@@ -10,4 +10,47 @@ Paspaudus mygtuką "Show users":
 Pastaba: Informacija apie user'į (jo kortelė) bei turi turėti bent minimalų stilių;
 -------------------------------------------------------------------------- */
 
-const ENDPOINT = 'https://api.github.com/users';
+const ENDPOINT = "https://api.github.com/users";
+
+const message = document.querySelector("#message");
+const button = document.querySelector("#btn");
+
+function getUserData() {
+  fetch(ENDPOINT)
+    .then((res) => res.json())
+    .then((data) => {
+      //   console.log(data);
+      message.remove();
+      for (i = 0; i < data.length; i++) {
+        const accName = data[i].login;
+        const accLogo = data[i].avatar_url;
+
+        resultTemplate(accName, accLogo);
+      }
+    });
+}
+
+button.addEventListener("click", () => getUserData());
+
+function resultTemplate(login, avatarUrl) {
+  const outputContainer = document.querySelector("#output");
+
+  const resultCont = document.createElement("div");
+  resultCont.setAttribute("class", "resultContainer");
+
+  const div = document.createElement("div");
+  const nickName = document.createElement("h4");
+  nickName.textContent = "User name:";
+  const loginResult = document.createElement("p");
+  loginResult.textContent = login;
+
+  const avatarResult = document.createElement("img");
+  avatarResult.src = avatarUrl;
+
+  div.append(nickName);
+  div.append(loginResult);
+  resultCont.append(div);
+  resultCont.append(avatarResult);
+
+  outputContainer.append(resultCont);
+}
