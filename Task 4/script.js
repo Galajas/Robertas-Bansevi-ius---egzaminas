9da7cbb266826d4c4c444f4eda9f8ed1ac667ae7
Robertas <galaja@inbox.lt>
2022-02-10 20:32:22 +0200

    Task 4 - 10.10

--- Task 4/script.js
+++ Task 4/script.js
@@ -9,4 +9,58 @@ Pastaba: Informacija apie automobilį (brand) (jo kortelė) bei turi turėti
 bent minimalų stilių;
 -------------------------------------------------------------------------- */
 
-const ENDPOINT = 'cars.json';
+const ENDPOINT = "./cars.json";
+
+function getcarsBrand() {
+  fetch(ENDPOINT)
+    .then((res) => res.json())
+    .then((data) => {
+      for (i = 0; i < data.length; i++) {
+        const brand = data[i].brand;
+        const brandId = brand.replace(/ /g, "");
+        const models = data[i].models;
+
+        resultTemplateForBrand(brand, brandId);
+
+        // console.log(brand);
+        // console.log(models);
+
+        for (j = 0; j < models.length; j++) {
+          const modelsName = models[j];
+
+          resultTemplateForcars(modelsName, brandId);
+
+          //   console.log(modelsName);
+        }
+      }
+    });
+}
+
+getcarsBrand();
+
+function resultTemplateForBrand(carsBrands, id) {
+  const outputContainer = document.querySelector("#output");
+
+  const brandContainer = document.createElement("div");
+
+  const brandName = document.createElement("h4");
+  brandName.textContent = `Car Brand: ${carsBrands}`;
+
+  const carsList = document.createElement("ul");
+  carsList.setAttribute("id", `brandContainer${id}`);
+  carsList.setAttribute("class", `carsList`);
+
+  brandContainer.append(brandName);
+  brandContainer.append(carsList);
+
+  outputContainer.append(brandContainer);
+}
+
+function resultTemplateForcars(carName, id) {
+  const carsList = document.querySelector(`#brandContainer${id}`);
+
+  const car = document.createElement("li");
+  car.textContent = `car name: ${carName}`;
+
+  carsList.append(car);
+}
