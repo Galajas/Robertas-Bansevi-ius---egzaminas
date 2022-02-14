2b35e55fb7bd78485c808ff71242d8ddd9962b75
Robertas <galaja@inbox.lt>
2022-02-14 10:45:51 +0200

    Task 9 - 02.14

--- Task 9/script.js
+++ Task 9/script.js
@@ -6,3 +6,23 @@ title, director, budget
 Metodas: 
 wasExpensive() - jeigu filmo budget bus didesnę nei 100 000 000 mln USD, tada gražins true, kiru atveju false 
 ------------------------------------------------------------------------------------------------------ */
+class Movie {
+  constructor(title, director, budget) {
+    this.title = title;
+    this.director = director;
+    this.budget = budget;
+  }
+
+  wasExpensive() {
+    const isOver = 1000000;
+    if (this.budget > isOver) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+}
+
+const newMovie = new Movie("Ice Age", `Chris Wedge`, 1500000);
+
+console.log(newMovie.wasExpensive());
